e84d0fe28275c4c376808829aa08dd77e0250f0e
tompagli <pagliuso.tom@gmail.com>
2023-07-24 17:58:56 -0300

    ID-Mode Refactoring

---
+++ samples/IDMode/Assertingwl-mTLS/models/PocData.go
@@ -0,0 +1,15 @@
+package models
+
+type PocData struct {
+	AccessToken            string                 `json:",omitempty"`
+	PublicKey              string                 `json:",omitempty"`
+	OauthSigValidation     *bool                  `json:",omitempty"`
+	OauthExpValidation     *bool                  `json:",omitempty"`
+	OauthExpRemainingTime  string                 `json:",omitempty"`
+	OauthClaims            map[string]interface{} `json:",omitempty"`
+	DASVIDToken            string                 `json:",omitempty"`
+	DASVIDClaims           map[string]interface{} `json:",omitempty"`
+	DasvidExpValidation    *bool                  `json:",omitempty"`
+	DasvidExpRemainingTime string                 `json:",omitempty"`
+	DasvidSigValidation    *bool                  `json:",omitempty"`
+}
