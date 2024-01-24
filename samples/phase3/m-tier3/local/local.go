04c64a5d4b6ebb93469fea317b4b9069dbe5923f
Lucas Cupertino <lcupertino@lcupertino.lcupertino>
2024-01-24 16:53:27 -0300

    Middle-tier 3 modifications

---
+++ samples/phase3/m-tier3/local/local.go
@@ -0,0 +1,30 @@
+package local
+
+import (
+	"log"
+
+	api "github.com/hpe-usp-spire/signed-assertions/phase3/api-libs/global"
+	alOps "github.com/hpe-usp-spire/signed-assertions/phase3/api-libs/options"
+
+	"github.com/hpe-usp-spire/signed-assertions/phase3/m-tier3/options"
+	"github.com/hpe-usp-spire/signed-assertions/phase3/m-tier3/monitoring-prom"
+)
+
+var Options *alOps.Options
+
+func init() {
+	monitor.RegisterMetrics()
+	log.Print("global init")
+
+	options, err := options.InitOptions()
+	if err != nil {
+		log.Fatal("Options init errored: ", err.Error())
+	}
+
+	Options = options
+}
+
+func InitGlobals() {
+	log.Print("init global")
+	api.InitGlobals(Options)
+}
---
+++ samples/phase3/m-tier3/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"context"
+
+	"github.com/hpe-usp-spire/signed-assertions/phase3/m-tier3/controller"
+	"github.com/hpe-usp-spire/signed-assertions/phase3/m-tier3/monitoring-prom"
+)
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go monitor.PrometheusAPI()
+	go monitor.UpdateMemoryUsage()
+	go monitor.UpdateCPUUsage()
+
+
+	controller.MiddleTierController(ctx)
+}
