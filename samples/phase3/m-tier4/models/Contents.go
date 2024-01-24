37be7ceb6e91ead3a74ac2e801654001cd37e856
Lucas Cupertino <lcupertino@lcupertino.lcupertino>
2024-01-24 16:58:13 -0300

    Middle-tier 4 modifications

---
+++ samples/phase3/m-tier4/models/Contents.go
@@ -0,0 +1,9 @@
+package models
+
+type Contents struct {
+	DasvidExpValidation    *bool  `json:",omitempty`
+	DasvidExpRemainingTime string `json:",omitempty`
+	DasvidSigValidation    *bool  `json:",omitempty`
+	DASVIDToken            string `json:",omitempty`
+	IDArtifacts			   string `json:",omitempty"`
+}
