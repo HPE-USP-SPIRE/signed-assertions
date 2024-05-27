927d38c7f636bbdbf9d06658a9708b3e0cc03fb6
henriquezanela <henrique_z_c@hotmail.com>
2024-05-27 01:42:57 -0300

    Remove .cfg files from tracking

--- scripts/reset_phase3_poc.sh
+++ scripts/reset_phase3_poc.sh
@@ -8,3 +8,5 @@ docker ps -a --filter "name=phase3_*" -q | xargs docker rm -f
 ##  REMOVE dangling images AND images related to Phase3
 docker image prune -f
 docker images --filter=reference='phase3_*' -q | xargs -r docker rmi
+
+bash kill_lsvid_spire.sh
